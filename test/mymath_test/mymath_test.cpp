59030c071676c2476efcef20014508394a56d6ae
Sam Warring <samwarring@gmail.com>
2023-06-27 15:28:41 -0700

    Fix build issues

--- test/mymath_test/mymath_test.cpp
+++ test/mymath_test/mymath_test.cpp
@@ -3,7 +3,7 @@
 
 TEST(MathTest, QuadraticRealRoots) {
   double a = 1, b = 0, c = -1;
-  auto [r1, r2] = project_lib::quadratic(a, b, c);
+  auto [r1, r2] = mymath::quadratic(a, b, c);
   auto lesser = r1.real() < r2.real() ? r1 : r2;
   auto greater = r1.real() < r2.real() ? r2 : r1;
   ASSERT_TRUE(lesser == -1.0);
@@ -12,14 +12,14 @@ TEST(MathTest, QuadraticRealRoots) {
 
 TEST(MathTest, QuadriaticRepeatedRoot) {
   double a = 1, b = 0, c = 0;
-  auto [r1, r2] = project_lib::quadratic(a, b, c);
+  auto [r1, r2] = mymath::quadratic(a, b, c);
   ASSERT_TRUE(r1 == 0.0);
   ASSERT_TRUE(r2 == 0.0);
 }
 
 TEST(MathTest, QuadraticImaginaryRoots) {
   double a = 1, b = 0, c = 1;
-  auto [r1, r2] = project_lib::quadratic(a, b, c);
+  auto [r1, r2] = mymath::quadratic(a, b, c);
   ASSERT_TRUE(r1.imag() != 0.0);
   ASSERT_TRUE(r2.imag() != 0.0);
 }
